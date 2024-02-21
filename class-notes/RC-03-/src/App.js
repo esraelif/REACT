ebe4eaeffed2b79f57c3b87343032f06cffdfbb6
esraelif <elifesratunca@gmail.com>
2024-02-21 10:32:15 +0100

    components

--- class-notes/RC-03-/src/App.js
+++ class-notes/RC-03-/src/App.js
@@ -1,24 +1,14 @@
 import "./App.css";
 
 import React from "react";
-import Msg from "./msg/Msg";
-import Footer from "./footer/Footer";
-
 
 function App() {
-
-
-
   return (
     <div>
-
-      <Msg />
-
-      <Footer />
-
+     
+      App
     </div>
   );
 }
 
 export default App;
-
