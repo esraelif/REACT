51ed924fdf5be035415e2a46a677069b58bd98ea
esraelif <elifesratunca@gmail.com>
2024-02-22 11:55:08 +0100

    sass

---
+++ class-notes/RC-04-SASS/src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss"
+
+import React from "react";
+
+function App() {
+  return (
+    <div>
+
+      App
+    </div>
+  );
+}
+
+export default App;
---
+++ class-notes/RC-04-reactbootstrap/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+
+import React from "react";
+
+//!bootstrap in yolunu import etmeliyiz, dilersek bunu index.js ye de yapabiliriz
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import dataA from "./data";
+
+import CourseCard from "./components/CourseCard";
+import MyNavbar from "./components/MyNavbar";
+import Slider from "./components/Slider";
+
+function App() {
+  return (
+    <div>
+      <MyNavbar />
+      <Slider />
+      <CourseCard veri={dataA} />
+    </div>
+  );
+}
+
+export default App;
