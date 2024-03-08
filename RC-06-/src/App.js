d9a62e1d62a5c6d1c4a22ca91f82947bbaa8ac28
esraelif <elifesratunca@gmail.com>
2024-03-08 09:36:18 +0100

    ButtonAdded

--- RC-06-/src/App.js
+++ RC-06-/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 
-import React from "react";
+import React, { useState } from "react";
+import LifeCycleMethods from "./components/LifeCycleMethods";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const [show, setShow] = useState(true)
+
   return (
-    <div>
-     
-      App
+    <div className="container text-center mt-4">
+      <button className="btn btn-danger" onClick={() => setShow(!show)}>SHOW</button>
+
+      {show && <LifeCycleMethods />}
+
+
     </div>
   );
 }
---
+++ RC-06-/src/components/LifeCycleMethods.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LifeCycleMethods = () => {
+    return (
+        <div>
+            LifeCycle
+        </div>
+    );
+}
+
+export default LifeCycleMethods;
