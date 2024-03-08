56e4ea525b7ad3433ef157dadac64afdef719395
esraelif <elifesratunca@gmail.com>
2024-03-08 10:14:40 +0100

    lifeCycleMethods

---
+++ RC-06-/src/components/Clock.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Clock = () => {
+
+    return (
+        <div>
+            <div>
+                <h2>****************************</h2>
+                <h1>
+
+                </h1>
+                <button className='btn btn-warning'>ARTTIR</button>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Clock
