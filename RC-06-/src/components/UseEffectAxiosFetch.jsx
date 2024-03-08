f446c89a2d22b79695f024cf55ea90842b188c1c
esraelif <elifesratunca@gmail.com>
2024-03-08 11:05:00 +0100

    ClockAdded

---
+++ RC-06-/src/components/UseEffectAxiosFetch.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const UseEffectAxiosFetch = () => {
+    return (
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div>
+            Use Effect
+        </div>
+    );
+}
+
+export default UseEffectAxiosFetch;
