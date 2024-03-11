de9d4f88b2303cc9313de638bf8f404f35c7ebbe
esraelif <elifesratunca@gmail.com>
2024-03-11 09:58:19 +0100

    components

--- class-notes/RC-07-useEffect-database/src/pages/Home.jsx
+++ class-notes/RC-07-useEffect-database/src/pages/Home.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import BilgiList from '../components/BilgiList';
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState([])
+  const url = 'https://tutorial-api.fullstack.clarusway.com/tutorials/';
+
+  //! GET(READ)
+
+  const getTutorials = async () => {
+    const veri = await axios.get(url)
+    // console.log(veri)
+    setTutorials(veri.data)
+
+
+  }
+
+  //!then zinciriyle de yazabilirsiniz
+  // useEffect(()=>{
+  // axios.get(url).then((res) => setTutorials(res.data))},[])}
+  useEffect(() => { getTutorials() }, [])
+
   return (
-    <div>Home</div>
+    <>
+
+      <BilgiList tutorials={tutorials} />
+    </>
   )
 }
 
