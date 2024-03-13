6befb57008ee69a03b97e2c25beaa714622e86db
esraelif <elifesratunca@gmail.com>
2024-03-13 11:08:32 +0100

    Button

--- RC-08-Router/src/components/Buton.jsx
+++ RC-08-Router/src/components/Buton.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
 
 const Buton = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>Buton</div>
+    <div className='text-center'>
+      <Button className='btn btn-danger p-4 m-2'>LOGIN</Button>
+      <Button className='btn btn-danger p-4 m-2'>CLOSE</Button>
+      <Button className='btn btn-danger p-4 m-2'>ERASE</Button>
+    </div>
   )
 }
 
