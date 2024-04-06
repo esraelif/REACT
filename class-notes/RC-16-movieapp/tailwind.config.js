911a472825e7ed99e2713568edeca0e064468904
esraelif <elifesratunca@gmail.com>
2024-04-06 21:47:19 +0200

    movieAppStarter

---
+++ class-notes/RC-16-movieapp/tailwind.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      "gray-dark-main": "#23242a",
+      "gray-dark-second": "#28292d",
+      "gray-light": "#d3dce6",
+      "red-main": "#ff4b45",
+    },
+  },
+  plugins: [],
+  darkMode: "selector"
+}
